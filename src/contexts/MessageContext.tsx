5b2c27c30637132912073c63c7b8625af8f40f3a
Elderkly <897676943@qq.com>
2024-05-20 14:29:28 +0800

    fix: build error

---
+++ src/contexts/MessageContext.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, useState, useContext, ReactNode } from 'react';
+import { Message, MessageContextType } from '../types/Chat';
+
+const MessageContext = createContext<MessageContextType | undefined>(undefined);
+
+export const MessageProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const addMessage = (message: Message) => {
+    setMessages(prevMessages => [...prevMessages, message]);
+  };
+
+  return (
+    <MessageContext.Provider value={{ messages, addMessage } as MessageContextType}>
+      {children}
+    </MessageContext.Provider>
+  );
+};
+
+export const useMessageContext = (): MessageContextType => {
+  const context = useContext(MessageContext);
+  if (!context) {
+    throw new Error('useMessageContext must be used within a MessageProvider');
+  }
+  return context;
+};
